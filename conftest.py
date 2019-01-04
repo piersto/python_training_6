1a3552b69678fe0d9ae1d3040244dc02deb97ab0
PStoiko <pstoiko@gmail.com>
2019-01-04 12:56:00 -0500

    Конфигурационный файл для описания тестируемого приложения

--- conftest.py
+++ conftest.py
@@ -1,21 +1,21 @@
 import pytest
 from fixture.application import Application
-
+import json
 fixture = None
+target = None
 
 
 @pytest.fixture
 def app(request):
     global fixture
+    global target
     browser = request.config.getoption('--browser')
-    base_url = request.config.getoption('--baseUrl')
-    secret_password = request.config.getoption('--secret_password')
-    if fixture is None:
-        fixture = Application(browser=browser, base_url=base_url, secret_password=secret_password)
-    else:
-        if not fixture.is_valid():
-            fixture = Application(browser=browser, base_url=base_url, secret_password=secret_password)
-    fixture.session.ensure_login(username="admin", password=secret_password)
+    if target is None:
+        with open(request.config.getoption('--target')) as config_file:
+            target = json.load(config_file)
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, base_url=target['base_url'])
+    fixture.session.ensure_login(username=target['username'], password=target['password'])
     return fixture
 
 
@@ -27,9 +27,8 @@ def stop(request):
     request.addfinalizer(fin)
     return fixture
 
+
 def pytest_addoption(parser):
     parser.addoption('--browser', action='store', default='chrome')
-    parser.addoption('--baseUrl', action='store', default='http://localhost/addressbook/')
-    parser.addoption('--secret_password', action='store', default='secret')
-
+    parser.addoption('--target', action='store', default='target.json')
 
--- fixture/application.py
+++ fixture/application.py
@@ -5,7 +5,7 @@ from fixture.contact import ContactHelper
 
 
 class Application:
-    def __init__(self, browser, base_url, secret_password):
+    def __init__(self, browser, base_url):
         if browser == 'chrome':
             self.wd = webdriver.Chrome()
         elif browser == 'firefox':
@@ -19,7 +19,6 @@ class Application:
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
         self.base_url = base_url
-        self.secret_password = secret_password
 #        self.wd.maximize_window()
 
     def is_valid(self):
@@ -29,7 +28,6 @@ class Application:
         except:
             return False
 
-
     def open_login_page(self):
         wd = self.wd
         wd.get(self.base_url)
@@ -37,7 +35,6 @@ class Application:
     def return_on_home_page(self):
         wd = self.wd
         wd.find_element_by_link_text("home page").click()
-
 
     def destroy(self):
         self.wd.quit()
