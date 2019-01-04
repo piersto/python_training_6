3b4fe4998da6eb0ace3da2d4280e1b996aec9f83
Pierre <pstoiko@gmail.com>
2019-01-03 21:51:03 -0500

    Added last lesson of the 5-th week.

--- fixture/application.py
+++ fixture/application.py
@@ -5,13 +5,20 @@ from fixture.contact import ContactHelper
 
 
 class Application:
-    def __init__(self):
-        self.wd = webdriver.Chrome()
-#        self.wd.maximize_window()
+    def __init__(self, browser, base_url):
+        if browser == 'chrome':
+            self.wd = webdriver.Chrome()
+        elif browser == 'firefox':
+            self.wd = webdriver.Firefox()
+        elif browser == 'ie':
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError('Unrecognized browser %s' % browser)
         self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -23,7 +30,7 @@ class Application:
 
     def open_login_page(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        wd.get(self.base_url)
 
     def return_on_home_page(self):
         wd = self.wd
